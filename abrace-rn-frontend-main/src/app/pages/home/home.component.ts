0bcaa170ddd07d18576f5459b556663dd90b9df7
DavigSo <davig4611@gmail.com>
2024-10-26 14:54:00 -0300

    implementação do envio de formulario voluntarie-se

--- abrace-rn-frontend-main/src/app/pages/home/home.component.ts
+++ abrace-rn-frontend-main/src/app/pages/home/home.component.ts
@@ -1,14 +1,14 @@
 import { BotaoComponent } from './../../components/botao/botao.component';
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
-import { BotaoPraCimaComponent } from '../../components/botao-pra-cima/botao-pra-cima.component';
+
 import { FooterComponent } from '../../components/footer/footer.component';
 import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, BotaoPraCimaComponent, FooterComponent, NgOptimizedImage, BotaoComponent],
+  imports: [HeaderComponent, FooterComponent, NgOptimizedImage, BotaoComponent],
   providers: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
