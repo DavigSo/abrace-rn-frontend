e5e764655e81f1233a22389ac02624c177b295e8
Luan <luanbfarias@outlook.com>
2024-11-25 09:40:45 -0300

    todas as alterações

--- abrace-rn-frontend-main/src/app/pages/galeria/galeria.component.ts
+++ abrace-rn-frontend-main/src/app/pages/galeria/galeria.component.ts
@@ -2,12 +2,36 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { NgOptimizedImage } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-galeria',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, NgOptimizedImage],
+  imports: [HeaderComponent, FooterComponent, NgOptimizedImage, CommonModule],
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.scss',
 })
-export class GaleriaComponent {}
+export class GaleriaComponent {
+  cards = [
+    { src: 'assets/img1.jpg', alt: 'Imagem 1', txt: 'Teste' },
+    { src: 'assets/img2.jpg', alt: 'Imagem 2', txt: 'Teste' },
+    { src: 'assets/img3.jpg', alt: 'Imagem 3', txt: 'Teste' },
+    { src: 'assets/img4.jpg', alt: 'Imagem 4', txt: 'Teste' },
+    { src: 'assets/img5.jpg', alt: 'Imagem 5', txt: 'Teste' },
+    { src: 'assets/img6.jpg', alt: 'Imagem 6', txt: 'Teste' },
+    { src: 'assets/img7.jpg', alt: 'Imagem 7', txt: 'Teste' },
+    { src: 'assets/img8.jpg', alt: 'Imagem 8', txt: 'Teste' },
+    { src: 'assets/img9.jpg', alt: 'Imagem 9', txt: 'Teste' },
+    { src: 'assets/img10.jpg', alt: 'Imagem 10', txt: 'Teste' },
+    { src: 'assets/img11.jpg', alt: 'Imagem 11', txt: 'Teste' },
+    { src: 'assets/img12.jpg', alt: 'Imagem 12', txt: 'Teste' },
+    { src: 'assets/img13.jpg', alt: 'Imagem 13', txt: 'Teste' },
+    { src: 'assets/img14.jpg', alt: 'Imagem 14', txt: 'Teste' },
+    { src: 'assets/img15.jpg', alt: 'Imagem 15', txt: 'Teste' },
+    { src: 'assets/img16.jpg', alt: 'Imagem 16', txt: 'Teste' },
+    { src: 'assets/img17.jpg', alt: 'Imagem 17', txt: 'Teste' },
+    { src: 'assets/img18.jpg', alt: 'Imagem 18', txt: 'Teste' },
+    { src: 'assets/img19.jpg', alt: 'Imagem 19', txt: 'Teste' },
+    { src: 'assets/img20.jpg', alt: 'Imagem 20', txt: 'Teste' },
+  ];
+}
--- abrace-rn-frontend-main/src/app/pages/lojinha/lojinha.component.ts
+++ abrace-rn-frontend-main/src/app/pages/lojinha/lojinha.component.ts
@@ -1,4 +1,4 @@
-import { NgOptimizedImage } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
@@ -6,8 +6,167 @@ import { FooterComponent } from '../../components/footer/footer.component';
 @Component({
   selector: 'app-lojinha',
   standalone: true,
-  imports: [NgOptimizedImage, HeaderComponent, FooterComponent],
+  imports: [NgOptimizedImage, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './lojinha.component.html',
   styleUrl: './lojinha.component.scss',
 })
-export class LojinhaComponent {}
+export class LojinhaComponent {
+  products = [
+    {
+      src: 'assets/produto1.jpg',
+      alt: 'Produto 1',
+      name: 'Produto 1',
+      price: 50,
+    },
+    {
+      src: 'assets/produto2.jpg',
+      alt: 'Produto 2',
+      name: 'Produto 2',
+      price: 80,
+    },
+    {
+      src: 'assets/produto3.jpg',
+      alt: 'Produto 3',
+      name: 'Produto 3',
+      price: 150,
+    },
+    {
+      src: 'assets/produto4.jpg',
+      alt: 'Produto 4',
+      name: 'Produto 4',
+      price: 40,
+    },
+    {
+      src: 'assets/produto5.jpg',
+      alt: 'Produto 5',
+      name: 'Produto 5',
+      price: 15,
+    },
+    {
+      src: 'assets/produto6.jpg',
+      alt: 'Produto 6',
+      name: 'Produto 6',
+      price: 90,
+    },
+    {
+      src: 'assets/produto7.jpg',
+      alt: 'Produto 7',
+      name: 'Produto 7',
+      price: 60,
+    },
+    {
+      src: 'assets/produto8.jpg',
+      alt: 'Produto 8',
+      name: 'Produto 8',
+      price: 25,
+    },
+    {
+      src: 'assets/produto9.jpg',
+      alt: 'Produto 9',
+      name: 'Produto 9',
+      price: 200,
+    },
+    {
+      src: 'assets/produto10.jpg',
+      alt: 'Produto 10',
+      name: 'Produto 10',
+      price: 70,
+    },
+    {
+      src: 'assets/produto11.jpg',
+      alt: 'Produto 11',
+      name: 'Produto 11',
+      price: 120,
+    },
+    {
+      src: 'assets/produto12.jpg',
+      alt: 'Produto 12',
+      name: 'Produto 12',
+      price: 30,
+    },
+    {
+      src: 'assets/produto13.jpg',
+      alt: 'Produto 13',
+      name: 'Produto 13',
+      price: 20,
+    },
+    {
+      src: 'assets/produto14.jpg',
+      alt: 'Produto 14',
+      name: 'Produto 14',
+      price: 110,
+    },
+    {
+      src: 'assets/produto15.jpg',
+      alt: 'Produto 15',
+      name: 'Produto 15',
+      price: 80,
+    },
+    {
+      src: 'assets/produto16.jpg',
+      alt: 'Produto 16',
+      name: 'Produto 16',
+      price: 180,
+    },
+    {
+      src: 'assets/produto17.jpg',
+      alt: 'Produto 17',
+      name: 'Produto 17',
+      price: 50,
+    },
+    {
+      src: 'assets/produto18.jpg',
+      alt: 'Produto 18',
+      name: 'Produto 18',
+      price: 250,
+    },
+    {
+      src: 'assets/produto19.jpg',
+      alt: 'Produto 19',
+      name: 'Produto 19',
+      price: 300,
+    },
+    {
+      src: 'assets/produto20.jpg',
+      alt: 'Produto 20',
+      name: 'Produto 20',
+      price: 45,
+    },
+    {
+      src: 'assets/produto21.jpg',
+      alt: 'Produto 21',
+      name: 'Produto 21',
+      price: 90,
+    },
+    {
+      src: 'assets/produto22.jpg',
+      alt: 'Produto 22',
+      name: 'Produto 22',
+      price: 60,
+    },
+    {
+      src: 'assets/produto23.jpg',
+      alt: 'Produto 23',
+      name: 'Produto 23',
+      price: 150,
+    },
+    {
+      src: 'assets/produto24.jpg',
+      alt: 'Produto 24',
+      name: 'Produto 24',
+      price: 70,
+    },
+    {
+      src: 'assets/produto25.jpg',
+      alt: 'Produto 25',
+      name: 'Produto 25',
+      price: 35,
+    },
+    {
+      src: 'assets/produto26.jpg',
+      alt: 'Produto 26',
+      name: 'Produto 26',
+      price: 120,
+    },
+  ];
+}
